74ab2894406d29be56649d8f525e56544624111f
Dilkhush Kumawat <dilkhushkumawat33@gmail.com>
2024-07-31 21:32:18 +0530

    follow/unfollow feature added in backend

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -2,6 +2,8 @@ import {User} from '../models/user.model.js'
 import jwt from 'jsonwebtoken'
 import {ApiError} from '../utils/ApiError.js'
 import {asyncHandler} from '../utils/asyncHandler.js'
+import { Post } from '../models/post.model.js'
+import mongoose from 'mongoose'
 const signupUser = async (req,res) => {
     const {email,password,firstName, lastName,username} = req.body
    
@@ -100,57 +102,92 @@ const logoutUser = asyncHandler(async (req, res) => {
 
 //showuser
 const showUser = asyncHandler(async (req, res) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(400).json({ message: 'Invalid user ID' });
+      }
     const user = await User.findById(req.params.id).select("-password -refreshToken");
     if (!user) {
         throw new ApiError(401, "User not found");
     }
-    res.json(user);
+    //posts
+    const posts = await Post.find({ owner: req.params.id });
+    res.json({ user, posts });
+    // res.json(user);
 });
 
 //followunfollow
+// const followUnfollow = asyncHandler(async (req, res) => {
+//     const user = await User.findById(req.params.id);
+//     if (!user) {
+//         throw new ApiError(401, "User not found");
+//     }
+//     //check if user is already following
+//     const isFollowing = await User.findOne({ _id: req.user._id, following: { $in: [req.params.id] } });
+//     //followers
+//     const isFollower = await User.findOne({ _id: req.params.id, followers: {
+//         $in: [req.user._id]
+//         }
+//         });
+       
+                        
+//      if (isFollowing) {
+//             //unfollow
+//         await User.findByIdAndUpdate(req.user._id, {
+//            $pull: {
+//             following: req.params.id 
+//         }
+//         },{ new: true });
+//         await User.findByIdAndUpdate(req.params.id, {
+//             $pull: {
+//                 followers: req.user._id
+//                 }
+//                 });
+//         res.json({ message: "Unfollowed" });
+//      } else {
+//                         //follow
+//          await User.findByIdAndUpdate(req.user._id, {
+//              $push: {
+//                      following: req.params.id
+//                   }
+//              },{ new: true }
+//         );
+//         await User.findByIdAndUpdate(req.params.id, {
+//             $push: {
+//                 followers: req.user._id
+//                 }
+//                 });
+//         res.json({ message: "Followed" });
+//          }
+//      })
+
+//addfollowingandfollower
 const followUnfollow = asyncHandler(async (req, res) => {
     const user = await User.findById(req.params.id);
     if (!user) {
-        throw new ApiError(401, "User not found");
+      throw new ApiError(401, "User not found");
     }
-    //check if user is already following
+  
     const isFollowing = await User.findOne({ _id: req.user._id, following: { $in: [req.params.id] } });
-    //followers
-    const isFollower = await User.findOne({ _id: req.params.id, followers: {
-        $in: [req.user._id]
-        }
-        });
-       
-                        
-     if (isFollowing) {
-            //unfollow
-        await User.findByIdAndUpdate(req.user._id, {
-           $pull: {
-            following: req.params.id 
-        }
-        },{ new: true });
-        await User.findByIdAndUpdate(req.params.id, {
-            $pull: {
-                followers: req.user._id
-                }
-                });
+  
+   
+    //   const session = await mongoose.startSession();
+    //   session.startTransaction();
+  
+      if (isFollowing) {
+        // Unfollow
+        await User.findByIdAndUpdate(req.user._id, { $pull: { following: req.params.id } });
+        await User.findByIdAndUpdate(req.params.id, { $pull: { followers: req.user._id } });
         res.json({ message: "Unfollowed" });
-     } else {
-                        //follow
-         await User.findByIdAndUpdate(req.user._id, {
-             $push: {
-                     following: req.params.id
-                  }
-             },{ new: true }
-        );
-        await User.findByIdAndUpdate(req.params.id, {
-            $push: {
-                followers: req.user._id
-                }
-                });
+      } else {
+        // Follow
+        await User.findByIdAndUpdate(req.user._id, { $push: { following: req.params.id } });
+        await User.findByIdAndUpdate(req.params.id, { $push: { followers: req.user._id } });
         res.json({ message: "Followed" });
-         }
-     })
+      }
+  
+     
+  });
+  
 
 
 
