64e785411e79e730309f809a3609a151f24eb9bd
Dilkhush Kumawat <dilkhushkumawat33@gmail.com>
2024-07-31 14:47:33 +0530

    comment feature added in backend

--- models/post.model.js
+++ models/post.model.js
@@ -3,30 +3,27 @@ import { User } from "./user.model.js";
 import { Schema } from "mongoose";
 
 const postSchema = new Schema({
-    caption:{
-        type:String,
+    caption: {
+        type: String,
     },
-    image:{
-        type:String,
-        required:true
-     },
-    owner:{
-        type:Schema.Types.ObjectId,
-        ref:'User',
+    image: {
+        type: String,
+        required: true
     },
-    likes:{
-        type:Number,
-        default:0
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
     },
-    Comments:{
-        type :[
-            {
-                type:Schema.Types.ObjectId,
-                ref:'Comment',
-            }
-        ],
-    }
-},{timestamps:true})
+    likes: {
+        type: Number,
+        default: 0
+    },
+    Comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment',
+        }
+    ]
+}, { timestamps: true });
 
-export const Post = mongoose.model("Post",postSchema)
-
+export const Post = mongoose.model("Post", postSchema);
