5c15e2571b8c88173ebb9c1987f5163d4710c19f
Dilkhush Kumawat <dilkhushkumawat33@gmail.com>
2024-07-31 19:07:01 +0530

    show user and follow feature added

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,8 @@
-import {signupUser,loginUser,logoutUser} from '../controller/user.controller.js'
+import {signupUser,loginUser,logoutUser,showUser,followUnfollow} from '../controller/user.controller.js'
 import express from 'express'
 const router = express.Router()
 import {upload} from '../middleware/multer.middleware.js'
+import {verifyJWT} from '../middleware/auth.middleware.js'
 router.get('/signup',(req,res) =>{
     console.log(req.user)
     res.render("./user/signup.ejs");
@@ -13,7 +14,8 @@ router.get('/login',(req,res) =>{
     
     res.render("./user/login.ejs");
 })
-
+router.get('/show/:id',showUser)
+router.post('/follow/:id', verifyJWT,followUnfollow)
 router.post('/login',loginUser)
 router.post('/logout',logoutUser)
 
